d0edb76617535528e1cd508740e7ef364bcc1d01
Adrian Furmanek <adrian.furm@gmail.com>
2021-06-04 02:42:58 +0200

    Defender and trophy
    Defender animation + arrow shoot, trophy animation, zombie movement

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    float currentSpeed = 1f;
+ 
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+    }
+    public void SetMovementSpeed(float speed)
+    {      
+        currentSpeed = speed;
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
