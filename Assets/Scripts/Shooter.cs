1ac8674a38d791cccf7ea088fc54ad9cc2585f56
Adrian Furmanek <adrian.furm@gmail.com>
2021-06-08 23:20:28 +0200

    New Defenders, Shoot when Enemy spawn
    Two new defenders Knight and Stone, Defenders Shoot only when enemies spawn on thier lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,7 +5,48 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
-    
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs (spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+    private bool IsAttackerInLane() 
+    {
+        if(myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position, transform.rotation);
