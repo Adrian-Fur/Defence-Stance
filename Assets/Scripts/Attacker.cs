992f239d56081b52cd85b2c22337f9039353f089
Adrian Furmanek <adrian.furm@gmail.com>
2021-06-09 23:03:15 +0200

    Attackers attack
    Attackers can attack and destroy defenders

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,42 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     float currentSpeed = 1f;
+    GameObject currentTarget;
  
 
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
+    }
+
     public void SetMovementSpeed(float speed)
     {      
         currentSpeed = speed;
+    }
+    public void Attack(GameObject target)
+    {
+            GetComponent<Animator>().SetBool("IsAttacking", true);
+            currentTarget = target;
+    }
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget)
+        {
+            return;
+        }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
     }
 }
